fa4d582f91a91596f186187143f5f3867dc3f090
Omkar571 <omkarnaik571@gmail.com>
2020-01-02 14:45:45 +0530

    Java Script and Type Script

---
+++ Front End Technologies/TYPE SCRIPT/trainees.js
@@ -0,0 +1,15 @@
+"use strict";
+var Training = /** @class */ (function () {
+    function Training() {
+    }
+    Training.prototype.printData = function () {
+    }; //end of method
+    return Training;
+}()); //end of class
+var trainerr1 = {
+    name: 'raja',
+    age: 12,
+    printData: function () {
+        console.log(this.name, this.age);
+    }
+};
